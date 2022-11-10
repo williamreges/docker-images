328ab81bd9369f5efcb5a70d85c42025b086e1ad
william reges <william.reges1986@gmail.com>
2022-11-10 00:53:42 -0300

    imagem ambiente kafka

--- rabbitmq-docker/docker-rabbitmq.sh
+++ rabbitmq-docker/docker-rabbitmq.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -p 15672:15672 -p 5672:5672 --hostname my-rabbit --name some-rabbit rabbitmq:3
+docker run -d -p 15672:15672 -p 5672:5672 --hostname my-rabbit --name some-rabbit -e RABBITMQ_DEFAULT_USER=user -e RABBITMQ_DEFAULT_PASS=password rabbitmq:3-management
